3157255783593943207e012e9a4855b4344292af
Thiago Cury <thiagocury@gmail.com>
2017-05-26 10:55:32 -0300

    Exemplo Criando a primeira aplicação em Android Kotlin - API19

---
+++ ExCriandoPrimeiraAplicacao/app/src/main/java/thiagocury/eti/br/excriandoprimeiraaplicacao/MainActivity.kt
@@ -0,0 +1,21 @@
+package thiagocury.eti.br.excriandoprimeiraaplicacao
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //Aqui serão inseridos os atributos da classe
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Aqui serão inseridos os códigos iniciais da aplicação.
+
+    }
+
+    /* Outras Funções(métodos) de utilização opcional.
+       Podemos criar as nossas próprias funções e/ou utilizar
+       as funções pré definidas da linguagem */
+}
